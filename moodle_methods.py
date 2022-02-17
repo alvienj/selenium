5767491c5b01f3fc9429c78c7e094f92640c4951
alvienj <99428780+alvienj@users.noreply.github.com>
2022-02-17 14:34:19 -0800

    Update moodle_methods.py

--- moodle_methods.py
+++ moodle_methods.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
+
 
 options = Options()
 options.add_arguement("--headless")
